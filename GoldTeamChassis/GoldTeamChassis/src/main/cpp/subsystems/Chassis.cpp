e484a02a29d6bcdf5d15474e71c3b83c66be39da
Kyle Kang <kylek@vandenrobotics.com>
2023-11-29 16:52:42 -0800

    Subsystem Created

---
+++ GoldTeamChassis/GoldTeamChassis/src/main/cpp/subsystems/Chassis.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Chassis.h"
+
+Chassis::Chassis(WPI_TalonFX& ID1, WPI_TalonFX& ID2, WPI_TalonFX& ID3, WPI_TalonFX& ID4)
+        : mFrontR(ID1),
+          mFrontL(ID2),
+          mBackR(ID3),
+          mBackL(ID4),
+          mDrive(mFrontR,mFrontL)
+          {
+            mBackR.Follow(mFrontR);
+    mBackL.Follow(mFrontL);
+          }
+
+// This method will be called once per scheduler run
+void Chassis::Periodic() {}
+
+void Chassis:TankDrive(double left, double right)
+{
+    mDrive.TankDrive(left, right);
+}
