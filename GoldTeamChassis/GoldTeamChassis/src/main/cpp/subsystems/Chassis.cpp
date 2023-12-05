c548f50df945a2066cccf9410a57d42162ec7dd8
Kyle Kang <kylek@vandenrobotics.com>
2023-12-05 15:58:27 -0800

    Added thing for Driver station
    wheel velocity thing

--- GoldTeamChassis/GoldTeamChassis/src/main/cpp/subsystems/Chassis.cpp
+++ GoldTeamChassis/GoldTeamChassis/src/main/cpp/subsystems/Chassis.cpp
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/Chassis.h"
+#include <frc/smartdashboard/SmartDashboard.h>
 
 Chassis::Chassis(WPI_TalonFX& ID1, WPI_TalonFX& ID2, WPI_TalonFX& ID3, WPI_TalonFX& ID4)
         : mFrontR(ID1),
@@ -16,7 +17,10 @@ Chassis::Chassis(WPI_TalonFX& ID1, WPI_TalonFX& ID2, WPI_TalonFX& ID3, WPI_Talon
           }
 
 // This method will be called once per scheduler run
-void Chassis::Periodic() {}
+void Chassis::Periodic() {
+  frc::SmartDashboard::PutNumber("RightSide", 10*mFrontR.GetSelectedSensorVelocity()*(2/3)/12);
+  frc::SmartDashboard::PutNumber("LeftSide", 10*mFrontL.GetSelectedSensorVelocity()*(2/3)/12);
+}
 
 void Chassis::TankDrive(double left, double right)
 {
